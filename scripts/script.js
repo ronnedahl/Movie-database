68a89812f796ef9aa6dec935998dfafff61394a1
Peter Andersson <karlstad88@hotmail.com>
2024-02-16 12:31:44 +0100

    Add to remote Github

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+window.addEventListener('load', () => {
+    console.log('load');
+    //Förslagsvis anropar ni era funktioner som skall sätta lyssnare, rendera objekt osv. härifrån
+    setupCarousel();
+});
+
+//Denna funktion skapar funktionalitet för karusellen
+function setupCarousel() {
+    console.log('carousel');
+    const buttons = document.querySelectorAll('[data-carousel-btn]');
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const offset = btn.dataset.carouselBtn === 'next' ? 1 : -1;
+            const slides = btn.closest('[data-carousel').querySelector('[data-slides');
+            const activeSlide = slides.querySelector('[data-active]');
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            
+            if(newIndex < 0) {
+                newIndex = slides.children.length - 1;
+            } else if( newIndex >= slides.children.length) {
+                newIndex = 0;
+            }
+
+            slides.children[newIndex].dataset.active = true;
+            delete activeSlide.dataset.active;
+        });
+    });
+}
